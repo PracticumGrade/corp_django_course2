eace963d6af58fd09c0e4b6114600a63637e5f14
aeksei <aeksei94@gmail.com>
2023-04-14 19:04:10 +0300

    test: add tests for config params

--- tests/test_admin_page.py
+++ tests/test_admin_page.py
@@ -5,14 +5,41 @@ from django.contrib import admin
 
 
 @pytest.mark.parametrize(
-    'app_label, model_name',
+    'app_label, model_name, config_attrs',
     [
-        ('courses', 'Category'),
-        ('courses', 'Course'),
-        ('lessons', 'Lesson'),
+        (
+            'courses',
+            'Category',
+            {
+                'list_display': ('title', 'description',),
+                'search_fields': ('title', 'description__contains',),
+                'list_display_links': ('title',),
+            },
+        ),
+        (
+            'courses',
+            'Course',
+            {
+                'list_display': ('title', 'author', 'category', 'is_published',),
+                'list_filter': ('category', 'is_published',),
+                'list_editable': ('is_published',),
+                'list_display_links': ('title',),
+                'readonly_fields': ('created_at', 'updated_at',),
+            }
+        ),
+        (
+            'lessons',
+            'Lesson',
+            {
+                'list_display': ('lesson_name', 'type', 'duration',),
+                'list_filter': ('type',),
+                'list_display_links': ('lesson_name',),
+                'list_editable': ('duration',)
+            },
+        ),
     ]
 )
-def test_admin_register(app_label, model_name):
+def test_admin_register(app_label, model_name, config_attrs):
     try:
         model = apps.get_model(app_label, model_name)
     except LookupError:
@@ -23,3 +50,11 @@ def test_admin_register(app_label, model_name):
         f'Убедитесь, что модель `{model._meta.object_name}` '
         'зарегистрирована в админке.'
     )
+
+    site = admin.site._registry[model]
+    for attr, value in config_attrs.items():
+        actual_value = getattr(site, attr)
+        assert actual_value == value, (
+            f'Убедитесь, что для модели `{model._meta.object_name}` '
+            f'в админке значение атрибута `{attr}` настроено согласно заданию.'
+        )
