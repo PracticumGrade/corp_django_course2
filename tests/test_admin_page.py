3bb7badde0342d64ed5d168347e5593ec1840273
aeksei <aeksei94@gmail.com>
2023-04-14 20:01:05 +0300

    test: add tests for config params inline admin views

--- tests/test_admin_page.py
+++ tests/test_admin_page.py
@@ -3,6 +3,8 @@ import pytest
 from django.apps import apps
 from django.contrib import admin
 
+from lessons.models import Lesson
+
 
 @pytest.mark.parametrize(
     'app_label, model_name, config_attrs',
@@ -57,4 +59,59 @@ def test_admin_register(app_label, model_name, config_attrs):
         assert actual_value == value, (
             f'Убедитесь, что для модели `{model._meta.object_name}` '
             f'в админке значение атрибута `{attr}` настроено согласно заданию.'
+        )
+
+
+def test_lesson_inline():
+    try:
+        from courses.admin import LessonInline
+    except ImportError:
+        raise AssertionError(
+            f'Убедитесь, что в файле `courses/admin.py` объявлен класс `LessonInline`'
+        )
+
+    app_label = 'courses'
+    model_name = 'Course'
+    try:
+        model = apps.get_model(app_label, model_name)
+    except LookupError:
+        raise AssertionError(
+            f'Убедитесь, что модель `{model_name}` объявлена в приложении `{app_label}`'
+        )
+    assert model in admin.site._registry, (
+        f'Убедитесь, что модель `{model._meta.object_name}` '
+        'зарегистрирована в админке.'
+    )
+
+    course_admin_site = admin.site._registry[model]
+
+    assert course_admin_site.inlines == (LessonInline, ), (
+        f'Убедитесь, что модель `LessonInline` настроена для отображения связанных записей'
+        'в админ представлении модели `Course`.'
+    )
+
+
+@pytest.mark.parametrize(
+    'expected_inline_class, config_attrs',
+    [
+        (admin.StackedInline, {'model': Lesson, 'extra': 0}),
+    ]
+)
+def test_lesson_inline_config(expected_inline_class, config_attrs):
+    try:
+        from courses.admin import LessonInline
+    except ImportError:
+        raise AssertionError(
+            f'Убедитесь, что в файле `courses/admin.py` объявлен класс `LessonInline`'
+        )
+
+    assert issubclass(LessonInline, expected_inline_class), (
+        f'Убедитесь, что в файле `courses/admin.py` класс `LessonInline` унаследован согласно заданию.'
+    )
+
+    for attr, value in config_attrs.items():
+        actual_value = getattr(LessonInline, attr)
+        assert actual_value == value, (
+            f'Убедитесь, что в файле `courses/admin.py` для класса `LessonInline` '
+            f'значение атрибута `{attr}` настроено согласно заданию.'
         )
