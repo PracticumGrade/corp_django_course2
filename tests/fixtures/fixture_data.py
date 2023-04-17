094fc62659465dcb84cfef1d818532de9795216f
aeksei <aeksei94@gmail.com>
2023-04-17 12:18:10 +0300

    test: add tests for n plus on problem and lesson name field

--- tests/fixtures/fixture_data.py
+++ tests/fixtures/fixture_data.py
@@ -2,7 +2,15 @@ import pytest
 from django.contrib.auth import get_user_model
 
 
+N_LESSONS = 10
+
+
 @pytest.fixture
 def author(mixer):
     User = get_user_model()
     return mixer.blend(User)
+
+
+@pytest.fixture
+def lessons(mixer):
+    return mixer.cycle(N_LESSONS).blend('lessons.Lesson')
